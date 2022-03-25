765909380975e4ceb78413af07474b277b551a28
Daniel Brechner <brechner.daniel@gmail.com>
2022-03-25 08:45:59 -0400

    added website counter

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ function validateForm() {
     }
     document.write("Thank you for contacting me! Expect a response soon.");
 }
+
+//Website visit counter
+
+let counterContainer = document.querySelector(".website-counter");
+
+let visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
